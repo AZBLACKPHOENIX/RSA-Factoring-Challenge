74b69eac9eac205a94d6cb255fc95f5f19a065ad
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-18 13:27:30 -0800

    factors

--- factors.c
+++ factors.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
-void factorize(int num) {
-    int i;
-
-    for (i = 2; i <= num / 2; i++) {
+// Function to check if a number is prime
+int isPrime(int num) {
+    if (num < 2) {
+        return 0; // Not prime
+    }
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
+            return 0; // Not prime
+        }
+    }
+    return 1; // Prime
+}
+
+// Function to factorize a number
+void factorize(int num) {
+    for (int i = 2; i <= num / 2; i++) {
+        if (isPrime(i) && isPrime(num / i)) {
             printf("%d=%d*%d\n", num, i, num / i);
             return;
         }
     }
-
     printf("%d=%d*%d\n", num, num, 1);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         return 1;
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
+    if (!file) {
         perror("Error opening file");
         return 1;
     }
@@ -31,7 +42,6 @@ int main(int argc, char **argv) {
     }
 
     fclose(file);
-
     return 0;
 }
 
